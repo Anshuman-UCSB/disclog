8c48b0031d93d1551739a608e12757100ef4e48f
Anshuman Dash <anshuman@ucsb.edu>
2024-10-15 17:41:08 -0700

    Pivoting and updating tests

--- tests/test_disclog.py
+++ tests/test_disclog.py
@@ -1,5 +1,9 @@
 import disclog
+import toml
 
 
 def test_version():
-    assert disclog.__version__ == "0.1.0"
+    with open("pyproject.toml", "r") as f:
+        pyproject = toml.load(f)
+        version = pyproject["tool"]["poetry"]["version"]
+    assert disclog.__version__ == version
